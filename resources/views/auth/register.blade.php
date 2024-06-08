88dc1d19b28fce7e3d5d242dd2ed5f27d8130a5f
Miloud Ajouamy <ajouamymiloud@gmail.com>
2024-06-08 11:04:57 +0100

    end auth

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -24,64 +24,55 @@
     <div class="signup-section ptb-100">
         <div class="container">
             <div class="row">
+                @if ($errors->any())
+                    <div class="alert alert-danger">
+                        <ul>
+                            @foreach ($errors->all() as $error)
+                                <li>{{ $error }}</li>
+                            @endforeach
+                        </ul>
+                    </div>
+                @endif
                 <div class="col-lg-6 col-md-8 offset-md-2 offset-lg-3">
-                    <form class="signup-form">
+                    <form action="/register" method="POST" class="signup-form">
+                        @csrf
                         <div class="form-group">
-                            <label>Enter Username</label>
-                            <input type="text" class="form-control" placeholder="Enter Username" required>
+                            <label> Username </label>
+                            <input name="name" type="text" class="form-control" placeholder="Enter Username" required>
                         </div>
 
                         <div class="form-group">
-                          <label>Enter Email</label>
-                          <input type="email" class="form-control" placeholder="Enter Your Email" required>
+                            <label> Email</label>
+                            <input name="email" type="email" class="form-control" placeholder="Enter Your Email"
+                                required>
                         </div>
 
                         <div class="form-group">
-                            <label>Enter Password</label>
-                            <input type="password" class="form-control" placeholder="Enter Your Password" required>
+                            <label> Password</label>
+                            <input name="password" type="password" class="form-control" placeholder="Enter Your Password"
+                                required>
+                        </div>
+                        <div class="form-group">
+                            <label> Password</label>
+                            <input name="password_confirmation" type="password" class="form-control"
+                                placeholder="Enter Your Password" required>
                         </div>
 
                         <div class="signup-btn text-center">
                             <button type="submit">Sign Up</button>
                         </div>
 
-                        <div class="other-signup text-center">
-                            <span>Or sign up with</span>
-                            <ul>
-                                <li>
-                                    <a href="#">
-                                        <i class='bx bxl-google'></i>
-                                    </a>
-                                </li>
-                                <li>
-                                    <a href="#">
-                                        <i class='bx bxl-facebook'></i>
-                                    </a>
-                                </li>
-                                <li>
-                                    <a href="#">
-                                        <i class='bx bxl-twitter'></i>
-                                    </a>
-                                </li>
-                                <li>
-                                    <a href="#">
-                                        <i class='bx bxl-linkedin'></i>
-                                    </a>
-                                </li>
-                            </ul>
-                        </div>
-
                         <div class="create-btn text-center">
                             <p>
                                 Have an Account?
-                                <a href="signin.html">
+                                <a href="{{ route('login') }}">
                                     Sign In
                                     <i class='bx bx-chevrons-right bx-fade-right'></i>
                                 </a>
                             </p>
                         </div>
                     </form>
-                </div>  
+                </div>
             </div>
         </div>
     </div>
